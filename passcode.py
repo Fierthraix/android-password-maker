27d633fe49fdb738077ae6964556c50ab9a51639
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2020-03-15 16:50:47 -0400

    Fixed lints and trailing whitespace

--- passcode.py
+++ passcode.py
@@ -6,9 +6,9 @@ import argparse
 
 
 def print_grid(grid):
-    final_grid = []
     for i, row in enumerate(grid):
         print(' '.join(row))
+
 
 # Returns a coordinate list of unused neighbouring points
 def get_neighbours(point, grid, grid_size):
@@ -22,20 +22,19 @@ def get_neighbours(point, grid, grid_size):
             if 0 <= i < grid_size and 0 <= j < grid_size:
                 # Make sure point isn't used already
                 if grid[i][j] == 'x':
-                    neighbours.append([i,j])
+                    neighbours.append([i, j])
 
     return neighbours
 
+
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("grid_size", help="A number from 3 to 6", default="3", 
-            type=int)
-    ap.add_argument("pass_len", help="The password length (at least 3 segments)", 
-            default="4", type=int)
+    ap.add_argument("grid_size", help="A number from 3 to 6", default="3",
+                    type=int)
+    ap.add_argument("pass_len", help="The password length (at least 3 segments)",
+                    default="4", type=int)
 
     args = vars(ap.parse_args())
-
-
 
     grid_size = args["grid_size"]
     pass_len = args["pass_len"]
@@ -46,19 +45,17 @@ def main():
     if pass_len < 3 or pass_len > grid_size**2:
         sys.stderr.write("Your passphrase must be greater than 3 and not larger than the grid")
 
-
-
     # Produces a grid_size x grid_size matrix
-    grid = [ [ 'x' for i in range(grid_size) ] for i in range(grid_size) ]
+    grid = [['x' for i in range(grid_size)] for i in range(grid_size)]
 
     # Starting Point
-    point = [ random.randint(0, grid_size - 1), random.randint(0, grid_size - 1) ]
+    point = [random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)]
 
     # This loop builds the password and restarts if it gets trapped
     i = 0
     while True:
         # Update the grid with the next password point
-        grid[ point[0] ][ point[1] ] = str(i+1)
+        grid[point[0]][point[1]] = str(i+1)
 
         # Break if the password is the right length
         if i == pass_len - 1:
@@ -70,8 +67,8 @@ def main():
 
         # Restarts in case the pattern can't continue
         if len(neighbours) == 0:
-            grid = [ [ 'x' for i in range(grid_size) ] for i in range(grid_size) ]
-            point = [ random.randint(0, grid_size - 1), random.randint(0, grid_size - 1) ]
+            grid = [['x' for i in range(grid_size)] for i in range(grid_size)]
+            point = [random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)]
             i = -1
 
         # If there's only 1 option, take it
@@ -80,8 +77,10 @@ def main():
 
         # Choose a random neighbour as the next point
         else:
-            point = neighbours[ random.randint(0, len(neighbours) - 1) ]
+            point = neighbours[random.randint(0, len(neighbours) - 1)]
 
         i += 1
+
+
 if __name__ == '__main__':
     main()
